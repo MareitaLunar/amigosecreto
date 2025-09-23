9c411096cb68219681f1bb6b1c58135f72cfdc6a
MareaLunar <sagitariamaria@gmail.com>
2025-09-22 21:06:45 -0500

    Agregar Amigos ok

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listAmigos = [];
+
+
+
+function agregarAmigo() {
+    let amigo = document.getElementById('amigo').value;
+    
+    amigosList = document.getElementById('listaAmigos');
+    //validaciones de campo vacio
+    if (amigo === ""){
+        alert("Por favor ingrese un amigo");
+        return; 
+    }else{
+        if (listAmigos.includes(amigo)){
+            alert("Digite un amigo diferente");
+        }else{
+            listAmigos.push(amigo);
+            console.log(listAmigos);
+            amigosList.innerHTML += `<li>${amigo}</li>`;
+        }
+    }
+    document.getElementById('amigo').value = "";
+    
+}
+ 
